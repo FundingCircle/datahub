d1bfc998f11a8f65a6dbe31246e74e02469c385c
Louis Mackie <louis.mackie@fundingcircle.com>
2023-07-06 13:40:32 +0100

    Add jinja substitution for meta mapping

--- metadata-ingestion/src/datahub/utilities/mapping.py
+++ metadata-ingestion/src/datahub/utilities/mapping.py
@@ -3,6 +3,8 @@ import logging
 import operator
 import re
 from functools import reduce
+from jinja2 import Template
+from jinja2.sandbox import SandboxedEnvironment
 from typing import Any, Dict, List, Match, Optional, Union
 
 from datahub.emitter import mce_builder
@@ -34,8 +36,10 @@ def _insert_match_value(original_value: str, match_value: str) -> str:
     """
     If the original value is something like "foo{{ $match }}bar", then we insert the match value
     e.g. "foo<match_value>bar". Otherwise, it will leave the original value unchanged.
+    Uses a Jinja2 template render to perform simple string operations.
     """
-    return _match_regexp.sub(match_value, original_value)
+    return Template(_match_regexp.sub(match_value, original_value)).render(jinja_env=SandboxedEnvironment())
+
 
 
 class Constants:
