971d08bc52aed7a3ca659bdf4e28f7111663f886
Maiara Reinaldo <maiara.reinaldo@fundingcircle.com>
2023-07-17 16:34:48 +0100

    Add platform_instance to browse path for Tableau source and linting

--- metadata-ingestion/src/datahub/utilities/mapping.py
+++ metadata-ingestion/src/datahub/utilities/mapping.py
@@ -3,9 +3,10 @@ import logging
 import operator
 import re
 from functools import reduce
+from typing import Any, Dict, List, Match, Optional, Union
+
 from jinja2 import Template
 from jinja2.sandbox import SandboxedEnvironment
-from typing import Any, Dict, List, Match, Optional, Union
 
 from datahub.emitter import mce_builder
 from datahub.emitter.mce_builder import OwnerType
@@ -38,8 +39,9 @@ def _insert_match_value(original_value: str, match_value: str) -> str:
     e.g. "foo<match_value>bar". Otherwise, it will leave the original value unchanged.
     Uses a Jinja2 template render to perform simple string operations.
     """
-    return Template(_match_regexp.sub(match_value, original_value)).render(jinja_env=SandboxedEnvironment())
-
+    return Template(_match_regexp.sub(match_value, original_value)).render(
+        jinja_env=SandboxedEnvironment()
+    )
 
 
 class Constants:
